e3e444a9daf9eebb5ae087046bbb42334c98a4a2
EsmailELBoBDev2 <esmailelbob01124320019@pm.me>
2018-11-08 19:56:27 +0200

    ­

---
+++ Folder Locker and Unlocker PRO/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form12
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Lock"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(219, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Unlock"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox4.Controls.Add(Me.RadioButton3)
+        Me.GroupBox4.Controls.Add(Me.RadioButton4)
+        Me.GroupBox4.ForeColor = System.Drawing.Color.Black
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 90)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(75, 73)
+        Me.GroupBox4.TabIndex = 45
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Language"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(6, 42)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(51, 17)
+        Me.RadioButton3.TabIndex = 41
+        Me.RadioButton3.Text = "عربى"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Checked = True
+        Me.RadioButton4.Location = New System.Drawing.Point(6, 19)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton4.TabIndex = 40
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "English"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.Black
+        Me.GroupBox2.Location = New System.Drawing.Point(190, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(102, 72)
+        Me.GroupBox2.TabIndex = 44
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Always on top ?"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.RadioButton1.Location = New System.Drawing.Point(4, 42)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(38, 17)
+        Me.RadioButton1.TabIndex = 12
+        Me.RadioButton1.Text = "No"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Checked = True
+        Me.RadioButton2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.RadioButton2.Location = New System.Drawing.Point(4, 19)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(42, 17)
+        Me.RadioButton2.TabIndex = 11
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Yes"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 67)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(282, 17)
+        Me.ProgressBar1.TabIndex = 43
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(282, 20)
+        Me.TextBox1.TabIndex = 42
+        Me.TextBox1.Text = "Folder Path will show here"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.GroupBox1.Controls.Add(Me.RadioButton5)
+        Me.GroupBox1.Controls.Add(Me.RadioButton6)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Black
+        Me.GroupBox1.Location = New System.Drawing.Point(93, 90)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(91, 73)
+        Me.GroupBox1.TabIndex = 46
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Theme"
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(6, 42)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(48, 17)
+        Me.RadioButton5.TabIndex = 41
+        Me.RadioButton5.Text = "Light"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Checked = True
+        Me.RadioButton6.Location = New System.Drawing.Point(6, 19)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(47, 17)
+        Me.RadioButton6.TabIndex = 40
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Dark"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'Form12
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 170)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form12"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ Folder Locker and Unlocker PRO/Form1.vb
@@ -0,0 +1,210 @@
+﻿Imports System.IO
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports System.Timers
+Public Class Form12
+    Public status As String
+    Private arr As String() = New String(5) {}
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        RadioButton4.AutoCheck = True
+        RadioButton2.AutoCheck = True
+        Dim BuildNumber As String = My.Application.Info.Version.Build.ToString
+        Me.Text = "Folder Locker & Unlocker PRO - Build Number " + BuildNumber
+        status = ""
+        arr(0) = ".{2559a1f2-21d7-11d4-bdaf-00c04f60b9f0}"
+        arr(1) = ".{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+        arr(2) = ".{2559a1f4-21d7-11d4-bdaf-00c04f60b9f0}"
+        arr(3) = ".{645FF040-5081-101B-9F08-00AA002F954E}"
+        arr(4) = ".{2559a1f1-21d7-11d4-bdaf-00c04f60b9f0}"
+        arr(5) = ".{7007ACC7-3202-11D1-AAD2-00805FC1270E}"
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Lock Button
+        status = arr(0)
+
+
+
+        If FolderBrowserDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+
+
+            Dim d As DirectoryInfo = New DirectoryInfo(FolderBrowserDialog1.SelectedPath)
+
+            Dim selectedpath As String = d.Parent.FullName + d.Name
+
+            ProgressBar1.Value = 35
+
+            If FolderBrowserDialog1.SelectedPath.LastIndexOf(".{") = -1 Then
+
+
+
+                If (Not d.Root.Equals(d.Parent.FullName)) Then
+
+                    d.MoveTo(d.Parent.FullName & "\" & d.Name & status)
+
+                Else
+
+                    d.MoveTo(d.Parent.FullName + d.Name & status)
+
+                End If
+
+                TextBox1.Text = FolderBrowserDialog1.SelectedPath
+                ProgressBar1.Value = 70
+
+                ' PictureBox1.Image = Image.FromFile(Application.StartupPath & "\lock.jpg")
+                ProgressBar1.Value = 100
+            Else
+
+                status = getstatus(status)
+
+
+
+                d.MoveTo(FolderBrowserDialog1.SelectedPath.Substring(0, FolderBrowserDialog1.SelectedPath.LastIndexOf(".")))
+
+                TextBox1.Text = FolderBrowserDialog1.SelectedPath.Substring(0, FolderBrowserDialog1.SelectedPath.LastIndexOf("."))
+
+                ' PictureBox1.Image = Image.FromFile(Application.StartupPath & "\unlock.jpg")
+
+
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'UnLock Button
+        status = arr(1)
+
+
+
+        If FolderBrowserDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+
+
+            Dim d As DirectoryInfo = New DirectoryInfo(FolderBrowserDialog1.SelectedPath)
+
+            Dim selectedpath As String = d.Parent.FullName + d.Name
+
+
+
+            If FolderBrowserDialog1.SelectedPath.LastIndexOf(".{") = -1 Then
+
+
+
+                If (Not d.Root.Equals(d.Parent.FullName)) Then
+
+                    d.MoveTo(d.Parent.FullName & "\" & d.Name & status)
+
+                Else
+
+                    d.MoveTo(d.Parent.FullName + d.Name & status)
+
+                End If
+
+                TextBox1.Text = FolderBrowserDialog1.SelectedPath
+
+                'PictureBox1.Image = Image.FromFile(Application.StartupPath & "\lock.jpg")
+
+            Else
+
+                status = getstatus(status)
+
+
+
+                d.MoveTo(FolderBrowserDialog1.SelectedPath.Substring(0, FolderBrowserDialog1.SelectedPath.LastIndexOf(".")))
+
+                TextBox1.Text = FolderBrowserDialog1.SelectedPath.Substring(0, FolderBrowserDialog1.SelectedPath.LastIndexOf("."))
+
+                ' PictureBox1.Image = Image.FromFile(Application.StartupPath & "\unlock.jpg")
+
+
+
+            End If
+
+        End If
+    End Sub
+    Private Function getstatus(ByVal stat As String) As String
+
+        For i As Integer = 0 To 5
+
+            If stat.LastIndexOf(arr(i)) <> -1 Then
+
+                stat = stat.Substring(stat.LastIndexOf("."))
+
+            End If
+
+        Next i
+
+        Return stat
+
+    End Function
+
+    Private Sub RadioButton2_CheckedChanged_1(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If RadioButton2.Checked Then
+            TopMost = True
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            TopMost = False
+        End If
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Hide()
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        Button1.Text = "قفل"
+        Button2.Text = "فك القفل"
+        TextBox1.Text = "موقع الملف سيظهر هنا"
+        GroupBox4.Text = "اللغة"
+        GroupBox1.Text = "الشكل"
+        GroupBox2.Text = "دائما فى المقدمة ؟"
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        Button1.Text = "Lock"
+        Button2.Text = "UnLock"
+        TextBox1.Text = "Folder Path will show here"
+        GroupBox4.Text = "Language"
+        GroupBox1.Text = "Theme"
+        GroupBox2.Text = "Always on top ?"
+    End Sub
+
+    Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton6.CheckedChanged
+        Button1.BackColor = Color.Black
+        Button1.ForeColor = Color.White
+        Button2.BackColor = Color.Black
+        Button2.ForeColor = Color.White
+        GroupBox1.BackColor = Color.Black
+        GroupBox1.ForeColor = Color.White
+        GroupBox2.BackColor = Color.Black
+        GroupBox2.ForeColor = Color.White
+        GroupBox4.ForeColor = Color.White
+        GroupBox4.BackColor = Color.Black
+        TextBox1.ForeColor = Color.White
+        TextBox1.BackColor = Color.Black
+        Me.BackColor = Color.Black
+        Me.ForeColor = Color.White
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+        Button1.BackColor = Color.White
+        Button1.ForeColor = Color.Black
+        Button2.BackColor = Color.White
+        Button2.ForeColor = Color.Black
+        GroupBox1.BackColor = Color.White
+        GroupBox1.ForeColor = Color.Black
+        GroupBox2.BackColor = Color.White
+        GroupBox2.ForeColor = Color.Black
+        GroupBox4.ForeColor = Color.Black
+        GroupBox4.BackColor = Color.White
+        TextBox1.ForeColor = Color.Black
+        TextBox1.BackColor = Color.White
+        Me.BackColor = Color.White
+        Me.ForeColor = Color.White
+    End Sub
+End Class
+
